89545d1e8c0dbf0868021e1560f5ed86993a009b
user. <shl003@mail.usask.ca>
2024-12-16 11:19:14 -0600

    Add files via upload

--- app.py
+++ app.py
@@ -14,9 +14,10 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:4200",
+        "http://127.0.0.1:8080",
         "http://paymentappfrontend.s3-website.us-east-2.amazonaws.com/",
+        "http://paymentappfrontend.s3-website.us-east-2.amazonaws.com",
     ],
-    allow_credentials=True,
     allow_methods=["*"],  # Allow all methods (GET, POST, etc.)
     allow_headers=["*"],  # Allow all headers
 )
